00aabb80dc0f2438660fe8b1c14fa2ea1d076321
Joana Rodriguez Meza <122250029+cometajoe@users.noreply.github.com>
2025-06-03 23:49:13 +0000

    Fix logging of user request data in main.js

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ export default async ({ req, res, log, error }) => {
     // Log messages and errors to the Appwrite Console
     // These logs won't be seen by your end users
     log(`Total users: ${response.total}`);
+    log(`Request: ${JSON.stringify(req, null, 2)}`);
   } catch(err) {
     error("Could not list users: " + err.message);
   }
